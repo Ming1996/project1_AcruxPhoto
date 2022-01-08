e5d434c4e34e220cee635d3a044f45652505620c
Mingjia Xie <mingjia.xie@happyhackers.com.au>
2022-01-09 07:37:15 +1100

    update contact page

--- js/common.js
+++ js/common.js
@@ -1,20 +1,20 @@
 function fadeOutOnScroll(element) {
-    if (!element) {
-      return;
-    }
-    var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
-    var elementHeight = element.offsetHeight;
-    var scrollTop = document.documentElement.scrollTop;
-    var opacity = 1;
-    if (scrollTop > distanceToTop) {
-      opacity = 1 - (scrollTop - distanceToTop) / elementHeight - 0.6;
-    }
-  
-    if (opacity >= 0) {
-      element.style.opacity = opacity;
-    }
+  if (!element) {
+    return;
   }
-  // Fadeout Effect for title introduction section
+  var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+  var elementHeight = element.offsetHeight;
+  var scrollTop = document.documentElement.scrollTop;
+  var opacity = 1;
+  if (scrollTop > distanceToTop) {
+    opacity = 1 - (scrollTop - distanceToTop) / elementHeight - 0.6;
+  }
+  if (opacity >= 0) {
+    element.style.opacity = opacity;
+  }
+}
+// Fadeout Effect for title introduction section
 var homeTitle = document.getElementsByClassName("hero-intro")[0];
-
+var bottomTitle = document.getElementsByClassName("next-page-entry")[0];
 window.addEventListener("wheel", () => fadeOutOnScroll(homeTitle));
+// window.addEventListener("wheel", () => fadeOutOnScroll(bottomTitle));
