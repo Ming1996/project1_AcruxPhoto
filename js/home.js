e55c186aff93a8936b3dc4a9a9ff0386e41489b0
Mingjia Xie <mingjia.xie@happyhackers.com.au>
2022-01-09 02:06:42 +1100

    1. add portfolio html 2. structure file 3. add home page

---
+++ js/home.js
@@ -0,0 +1,87 @@
+function addEventListenerOnList(list, eventType, effect) {
+  list.forEach((element) => {
+    element.addEventListener(eventType, () => effect(element));
+  });
+}
+
+var showMenu = document.getElementsByClassName("show-menu")[0];
+var menuBackground = document.getElementsByClassName("nav-hide")[0];
+var menu = document.getElementsByClassName("nav-menu")[0];
+var logoDark = document.getElementsByClassName("logo-dark")[0];
+var logoWhite = document.getElementsByClassName("logo-white")[0];
+
+//Open main menu
+showMenu.addEventListener("click", () => {
+  showMenu.classList.contains("open")
+    ? showMenu.classList.remove("open")
+    : showMenu.classList.add("open");
+  menuBackground.style.backgroundColor =
+    menuBackground.style.backgroundColor == "black" ? "transparent" : "black";
+
+  menu.style.display = menu.style.display == "block" ? "none" : "block";
+
+  logoDark.style.display =
+    menuBackground.style.backgroundColor == "black" ? "none" : "block";
+  logoWhite.style.display =
+    menuBackground.style.backgroundColor == "black" ? "block" : "none";
+});
+// hover effect of main menu
+function menueHoverEffect(itemArray) {
+  addEventListenerOnList(itemArray, "mouseover", (element) => {
+    itemArray
+      .filter((e) => e !== element)
+      .forEach((otherElement) => {
+        otherElement.children[0].style.color = "grey";
+      });
+  });
+  addEventListenerOnList(itemArray, "mouseout", (element) => {
+    itemArray
+      .filter((e) => e !== element)
+      .forEach((otherElement) => {
+        otherElement.children[0].style.color = "white";
+      });
+  });
+}
+
+function fadeOutOnScroll(element) {
+  if (!element) {
+    return;
+  }
+  var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+  var elementHeight = element.offsetHeight;
+  var scrollTop = document.documentElement.scrollTop;
+  var opacity = 1;
+  if (scrollTop > distanceToTop) {
+    opacity = 1 - (scrollTop - distanceToTop) / elementHeight - 0.6;
+  }
+
+  if (opacity >= 0) {
+    element.style.opacity = opacity;
+  }
+}
+// Hover effect for main menu
+var menuItem = document.getElementsByClassName("menu-item");
+menueHoverEffect(Array.from(menuItem));
+
+// Open portfolio menu
+var portfolioMenu = document.getElementsByClassName("portfolio-menu")[0];
+document.getElementById("open-portfolio-menu").onclick = () => {
+  if (portfolioMenu.style.height == "0px" || !portfolioMenu.style.height) {
+    portfolioMenu.classList.add("portfolio-menu-opening");
+    portfolioMenu.classList.remove("portfolio-menu-closing");
+    portfolioMenu.style.height = "150px";
+  } else {
+    portfolioMenu.classList.remove("portfolio-menu-opening");
+    portfolioMenu.classList.add("portfolio-menu-closing");
+    portfolioMenu.style.height = 0;
+  }
+};
+
+// Hover effect for portfolio menu
+var portfolioItem = document.getElementsByClassName("portfolio-link");
+menueHoverEffect(Array.from(portfolioItem));
+
+// Fadeout Effect for home introduction section
+var homeTitle = document.getElementsByClassName("home-intro")[0];
+
+window.addEventListener("wheel", () => fadeOutOnScroll(homeTitle));
